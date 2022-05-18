a32c31e778ec821cb281b35f678364e4303f3e98
James Johnson <james.johnson@focusrite.com>
2022-05-18 12:26:50 +0100

    Pull resources on CMake configure

--- app/scripts/pull_resources.py
+++ app/scripts/pull_resources.py
@@ -1,9 +1,51 @@
-from numpy import source
+import argparse
 import boto3
 from botocore.client import Config
 from dotenv import load_dotenv
 import os
 from pprint import pprint
+
+
+def get_cli_args():
+    parser = argparse.ArgumentParser()
+
+    parser.add_argument('--access_key', type=str)
+    parser.add_argument('--secret_key', type=str)
+    parser.add_argument('--repo_directory', type=str)
+
+    return parser.parse_args()
+
+
+def get_access_key():
+    key = os.getenv('aws_access_key_id')
+
+    if key is not None:
+        return key
+
+    args = get_cli_args()
+
+    try:
+        return args.access_key
+    except:
+        pass
+
+    return input('AWS Access Key: ')
+
+
+def get_secret_key():
+    key = os.getenv('aws_secret_access_key')
+
+    if key is not None:
+        return key
+
+    args = get_cli_args()
+
+    try:
+        return args.secret_key
+    except:
+        pass
+
+    return input('AWS Secret Key: ')
 
 
 def create_parameters():
@@ -12,8 +54,8 @@ def create_parameters():
     return {
         'bucket': 'hackday10.desktop.cms',
         'region': 'eu-west-2',
-        'access_key': os.getenv('aws_access_key_id'),
-        'secret_key': os.getenv('aws_secret_access_key'),
+        'access_key': get_access_key(),
+        'secret_key': get_secret_key(),
     }
 
 
@@ -35,14 +77,21 @@ if __name__ == '__main__':
 
     response = s3_client.list_objects_v2(Bucket=parameters['bucket'])
 
+    repo_dir = get_cli_args().repo_directory
+
     for object in response['Contents']:
         object_key = object['Key']
-        destination_file = 'app/resources/' + object_key
+        destination_file = os.path.join(repo_dir,
+                                        'resources',
+                                        object_key)
 
         print('Downloading "' + object_key +
               '" to "' + destination_file + '"...')
 
-        with open(destination_file, 'wb') as file:
+        if not os.path.exists(destination_file):
+            os.makedirs(os.path.dirname(destination_file))
+
+        with open(destination_file, 'wb+') as file:
             s3_client.download_file(
                 parameters['bucket'],
                 object_key,
